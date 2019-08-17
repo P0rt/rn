f4358cc434dfcbc826fb29b6a3cda42acbf5a727
p00rt <serzhooo@gmail.com>
2019-08-17 21:55:48 +0300

    First components: fixed text Age

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+const ComponentsScreen = () => {
+    const name = 'Kirill';
+    return (
+        <View>
+            <Text style={styles.textStyle}>Я начал шлепать на реакт нэйтив</Text>
+            <Text style={styles.subHeaderStyle}>Меня зовут {name}</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 45
+    },
+    subHeaderStyle: {
+        fontSize: 20        
+    }
+});
+
+export default ComponentsScreen;
---
+++ src/screens/ListScreen.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Text, StyleSheet, View, FlatList } from 'react-native';
+
+const ListScreen = () => {
+
+    const friends = [
+        {name: 'Friend 1', age: '20'},
+        {name: 'Friend 2', age: '45'},
+        {name: 'Friend 3', age: '32'},
+        {name: 'Friend 4', age: '27'},
+        {name: 'Friend 5', age: '53'},
+        {name: 'Friend 6', age: '30'}
+    ];
+
+    return (
+        <FlatList 
+            keyExtractor={friend => friend.name}
+            data={friends} 
+            renderItem={({item}) => {
+                return (
+                    <Text style={styles.text}>{item.name} - Age {item.age}</Text>
+                )
+            }}
+        />    
+    );
+}
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 40,
+        marginVertical: 50
+    }
+});
+
+export default ListScreen;
