3de38f58c339506e0b854b34875c3c418ccb2ce4
p00rt <serzhooo@gmail.com>
2019-08-18 19:45:59 +0300

    added comments

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,14 +1,15 @@
-import React, {useReducer} from 'react';
+// Этот компонент создан с Reducer — это просто функция, которая меняет состояния объекта в state, делая его копию и обращается всегда к копии;
+
+import React, { useReducer } from 'react'; // подключили Reducer;
 import { Text, StyleSheet, View } from 'react-native';
 import ColorCounter from '../components/ColorCounter'
 
 const COLOR_INCREMENT = 15;
 
-const reducer = (state, action) => {
-
+const reducer = (state, action) => { // action
     switch (action.colorToChange) {
         case 'red': 
-            return { ...state, red: state.red + action.amount};
+            return { ...state, red: state.red + action.amount}; // тут просто скопировали стэйт ...state оператором spread, присвоили цвету сумму
         case 'green': 
             return { ...state, green: state.green + action.amount};
         case 'blue': 
@@ -17,12 +18,15 @@ const reducer = (state, action) => {
 };
 
 const SquareScreen = () => {
-    const [state, dispatch] = useReducer(reducer, {red: 0, green: 0, blue:0});
+    const [state, dispatch] = useReducer(reducer, {red: 0, green: 0, blue:0}); 
+    // первый аргумент state, второй это dispatch — это функция по запуску редьюса
+    // синтаксис useReducer(reducer, вторым аргументом наш объект с цветами) 
     const {red, green, blue} = state;
+    // добавляем переменные в state
     return (    
         <View>
             <ColorCounter 
-                onIncrease={() => dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT })}
+                onIncrease={() => dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT })} // вызываем dispatch с аргументами, цвет который нужно изменить и сумма
                 onDecrease={() => dispatch({ colorToChange: 'red', amount: -1 * COLOR_INCREMENT })}
                 color="Red" 
             />
@@ -38,9 +42,8 @@ const SquareScreen = () => {
             />
             <View
                 style={{
-                    height: 100,
-                    width: 200,
-                    backgroundColor: `rgb(${state.red},${state.green},${state.blue})`
+                    height: 1000,
+                    backgroundColor: `rgb(${state.red},${state.green},${state.blue})` // берем цвета из state
                 }}
             />
         </View>
