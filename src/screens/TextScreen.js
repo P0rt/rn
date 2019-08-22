0b6a776b5100a7db7c050d49f4d1265b60819113
p00rt <serzhooo@gmail.com>
2019-08-22 18:50:36 +0300

    added new component TextScreen and added validation

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,15 +1,40 @@
 import React, { useState } from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, View, TextInput } from 'react-native';
 
 const TextScreen = () => {
+    const [password, setPassword] = useState('');
 
     return (
         <View>
-            <Text>Kek</Text>
+            <Text style={styles.input}>Enter Password:</Text>
+            <TextInput 
+                style={styles.input} 
+                autoCapitalaize="none"
+                autoCorrect={false}
+                value={password}
+                onChangeText={newValue => setPassword(newValue)}
+            />
+            {
+                password.length < 4
+                    ? <Text>Пароль должен быть больше пяти символов</Text> 
+                    : null
+            }
         </View>
     );
 }
 
-const styles = StyleSheet.create({ });
+const styles = StyleSheet.create({ 
+    input: {
+        margin: 15,
+        padding: 5, 
+        height: 50,
+        fontSize: 30,
+        borderColor: 'red',
+        borderWidth: 1
+    },
+    input: {
+        fontSize: 30
+    }
+});
 
 export default TextScreen;
